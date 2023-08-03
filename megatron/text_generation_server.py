c33817534d7885ea76112122636a2c7bbc685701
Klaudia Thellmann <5738212+KlaudiaTH@users.noreply.github.com>
2023-08-03 11:27:52 +0200

    Removed some comments from text_generation_server.py

--- megatron/text_generation_server.py
+++ megatron/text_generation_server.py
@@ -29,8 +29,7 @@ class MegatronGenerate(Resource):
         choice = torch.cuda.LongTensor([BEAM_NUM])
         torch.distributed.broadcast(choice, 0)
 
-# Der Generierungs-Request wird hier verarbeitet
-# Parameter kommen als JSON rein und werden zu dicts geparst
+
     def put(self):
         args = get_args()
 
@@ -56,7 +55,7 @@ class MegatronGenerate(Resource):
         tokens_to_generate = (
             64  # Choosing hopefully sane default.  Full sequence is slow
         )
-# Anzahl Tokens die zu generieren sind
+
         if "tokens_to_generate" in request.get_json():
             tokens_to_generate = request.get_json()["tokens_to_generate"]
             if not isinstance(tokens_to_generate, int):
@@ -65,8 +64,7 @@ class MegatronGenerate(Resource):
                 return (
                     "tokens_to_generate must be an integer greater than or equal to 0"
                 )
-# Ob die logprobs der Tokensequenzen zurückgegeben werden sollen
-# Nur die logprobs der generierten und Eingabetoken
+
         logprobs = False
         if "logprobs" in request.get_json():
             logprobs = request.get_json()["logprobs"]
@@ -75,7 +73,7 @@ class MegatronGenerate(Resource):
 
         if tokens_to_generate == 0 and not logprobs:
             return "tokens_to_generate=0 implies logprobs should be True"
-# Temperature sampling
+
         temperature = 1.0
         if "temperature" in request.get_json():
             temperature = request.get_json()["temperature"]
@@ -87,7 +85,7 @@ class MegatronGenerate(Resource):
                 return (
                     "temperature must be a positive number less than or equal to 100.0"
                 )
-# Für das Beam-Search die top_k
+
         top_k = 0.0
         if "top_k" in request.get_json():
             top_k = request.get_json()["top_k"]
@@ -95,7 +93,7 @@ class MegatronGenerate(Resource):
                 return "top_k must be an integer equal to or greater than 0 and less than or equal to 1000"
             if not (0 <= top_k <= 1000):
                 return "top_k must be equal to or greater than 0 and less than or equal to 1000"
-# Für Sampling
+
         top_p = 0.0
         if "top_p" in request.get_json():
             top_p = request.get_json()["top_p"]
@@ -105,7 +103,7 @@ class MegatronGenerate(Resource):
                 return "cannot set both top-k and top-p samplings."
             if not (0 <= top_p <= 1.0):
                 return "top_p must be less than or equal to 1.0"
-# Für Sampling
+
         top_p_decay = 0.0
         if "top_p_decay" in request.get_json():
             top_p_decay = request.get_json()["top_p_decay"]
@@ -115,7 +113,7 @@ class MegatronGenerate(Resource):
                 return "top_p_decay cannot be set without top_p"
             if not (0 <= top_p_decay <= 1.0):
                 return "top_p_decay must be less than or equal to 1.0"
-# Für Sampling
+
         top_p_bound = 0.0
         if "top_p_bound" in request.get_json():
             top_p_bound = request.get_json()["top_p_bound"]
@@ -127,7 +125,7 @@ class MegatronGenerate(Resource):
                 return "top_p_bound cannot be set without top_p"
             if not (0.0 < top_p_bound <= top_p):
                 return "top_p_bound must be greater than 0 and less than top_p"
-# Wenn true würde das ein EOD Token vorne anfügen
+
         add_BOS = False
         if "add_BOS" in request.get_json():
             add_BOS = request.get_json()["add_BOS"]
@@ -137,7 +135,6 @@ class MegatronGenerate(Resource):
         if any([len(prompt) == 0 for prompt in prompts]) and not add_BOS:
             return "Empty prompts require add_BOS=true"
         
-# Verschiedene Flags mit denen man die Generierung steuern kann
         stop_on_double_eol = False
         if "stop_on_double_eol" in request.get_json():
             stop_on_double_eol = request.get_json()["stop_on_double_eol"]
@@ -182,7 +179,6 @@ class MegatronGenerate(Resource):
             if len(prompts) > 1:
                 return "When doing beam_search, batch size must be 1"
 
-        # TODO Read from tokenizer
         stop_token = 3
         if "stop_token" in request.get_json():
             stop_token = request.get_json()["stop_token"]
@@ -236,11 +232,7 @@ class MegatronGenerate(Resource):
                         response_logprobs,
                         response_tokens,
                         response_is_max_logprobs,
-                        # loglikelihood: Kriegt eine Seq. von Token rein und berechnet
-                        # für jedes Token die vorhergesagte Wahrsch.
-                        # und zusätzlich die Information ob es greedy generiert werden hätte können
-                        # greedy_until: Geg einen Kontext generiere weiter
-                    ) = generate_and_post_process( # Hier ist der Aufruf: Für loglikelihood als auch für greedy until
+                    ) = generate_and_post_process( 
                         self.model,
                         prompts=prompts,
                         tokens_to_generate=tokens_to_generate,
@@ -256,10 +248,10 @@ class MegatronGenerate(Resource):
                         stop_on_eol=stop_on_eol,
                         prevent_newline_after_colon=prevent_newline_after_colon,
                         random_seed=random_seed,
-                        return_is_max_logprobs=True # für die Information ob ein Token greedy hätte generiert werden können
+                        return_is_max_logprobs=True
                     )
 
-                    result = { # Das Format in dem die Response an den Client zurückgeht
+                    result = { 
                         "text": response,
                         "segments": response_seg,
                         "logprobs": response_logprobs,
@@ -272,7 +264,7 @@ class MegatronGenerate(Resource):
                 return ve.args[0]
             print("end time: ", datetime.datetime.now())
 
-# Eigene Implementierungen für Tokenization und Detokenization
+
 class MegatronTokenizer(Resource):
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
@@ -319,7 +311,7 @@ class MegatronDetokenizer(Resource):
     def options():
         return jsonify({})
 
-# Router worüber die HTTP-Anfragen an die für die jeweilige URL zuständigen Klassen weitergeleitet werden
+
 class MegatronServer(object):
     def __init__(self, model):
         self.app = Flask(__name__, static_url_path="")
