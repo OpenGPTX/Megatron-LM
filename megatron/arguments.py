9b3c47217a0c6e5bf704d861d1dea3988f26436c
janEbert <janpublicebert@posteo.net>
2023-08-16 14:48:50 +0200

    Add per-attention-head scaling
    Like in NormFormer. https://arxiv.org/abs/2110.09456
    
    Each attention head's output is scaled by a learned scalar.

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -613,6 +613,10 @@ def _add_network_size_args(parser):
                        action='store_true',
                        help='If set, use original BERT residula connection '
                        'ordering.')
+    group.add_argument('--scale-heads',
+                       action='store_true',
+                       help="Whether to scale each attention head's output by "
+                       'a learned scalar.')
     group.add_argument('--openai-gelu', action='store_true',
                        help='Use OpenAIs GeLU implementation. This option'
                        'should not be used unless for backward compatibility'
